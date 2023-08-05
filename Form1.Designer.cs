724d3cc436230565fe1e6c895cc4876e27aa308c
tomasvicar <tomasvicar@gmail.com>
2023-08-05 13:07:17 +0200

    hotovo

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace metej_stepper_control
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label_COM = new System.Windows.Forms.Label();
+            this.listBox_comunication = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(53, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "0";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(53, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "x";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(151, 121);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(69, 23);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label_COM
+            // 
+            this.label_COM.AutoSize = true;
+            this.label_COM.Location = new System.Drawing.Point(151, 164);
+            this.label_COM.Name = "label_COM";
+            this.label_COM.Size = new System.Drawing.Size(24, 15);
+            this.label_COM.TabIndex = 5;
+            this.label_COM.Text = "NA";
+            this.label_COM.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox_comunication
+            // 
+            this.listBox_comunication.FormattingEnabled = true;
+            this.listBox_comunication.ItemHeight = 15;
+            this.listBox_comunication.Location = new System.Drawing.Point(303, 43);
+            this.listBox_comunication.Name = "listBox_comunication";
+            this.listBox_comunication.Size = new System.Drawing.Size(295, 289);
+            this.listBox_comunication.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 346);
+            this.Controls.Add(this.listBox_comunication);
+            this.Controls.Add(this.label_COM);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private NumericUpDown numericUpDown1;
+        private Label label_COM;
+        private ListBox listBox_comunication;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+using System.IO.Ports;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace metej_stepper_control
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            openPumpSerial();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            openPumpSerial();
+        }
+
+
+        public void openPumpSerial()
+        {
+            this.label_COM.Text = "Connecting";
+            this.label_COM.ForeColor = System.Drawing.Color.Gray;
+            this.label_COM.Refresh();
+
+            var ArrayComPortsNames = SerialPort.GetPortNames();
+
+
+
+            if (!(serialPort == null))
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                    Thread.Sleep(3000);
+                }
+
+            }
+
+
+
+            foreach (string port in ArrayComPortsNames)
+            {
+
+
+
+
+
+                try
+                {
+
+
+
+                    Console.WriteLine(port);
+                    serialPort = new SerialPort(port, 9600);
+
+
+
+                    serialPort.Open();
+
+
+                    serialPort.WriteTimeout = 500;
+                    serialPort.ReadTimeout = 2000;
+
+                    serialPort.WriteLine("ok?");
+
+
+
+                    string message = serialPort.ReadLine();
+                    Console.WriteLine(message);
+
+                    if (message.Trim() == "ok")
+                    {
+                        this.label_COM.Text = port;
+
+                        this.label_COM.ForeColor = System.Drawing.Color.Green;
+
+                        serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+
+
+                    }
+                    else
+                    {
+                        serialPort.Close();
+
+                    }
+
+                }
+                catch (TimeoutException)
+                {
+                    Console.WriteLine("time out");
+                    serialPort.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.ToString());
+                    serialPort.Close();
+                }
+            }
+
+            if (this.label_COM.Text == "Connecting")
+            {
+                Console.WriteLine("COM step 10");
+
+
+                this.label_COM.Text = "NA";
+
+                this.label_COM.ForeColor = System.Drawing.Color.Red;
+            }
+
+            Console.WriteLine("COM step 11");
+
+
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SerialPort sp = (SerialPort)sender;
+                string indata = sp.ReadLine();
+
+                Console.WriteLine(indata);
+
+
+
+                string to_write = DateTime.Now.ToString("HH:mm:ss.fff");
+
+                this.listBox_comunication.BeginInvoke((MethodInvoker)delegate
+                {
+                    this.listBox_comunication.BeginUpdate();
+                    this.listBox_comunication.Items.Add(to_write + ": " + indata);
+                    if (this.listBox_comunication.Items.Count > 10)
+                    {
+                        this.listBox_comunication.Items.RemoveAt(0);
+                    }
+                    this.listBox_comunication.EndUpdate();
+                });
+
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine(button1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine(button2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine(button3.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine(button4.Text);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            serialPort.WriteLine("t" + ((int)numericUpDown1.Value).ToString());
+        }
+    }
+}
